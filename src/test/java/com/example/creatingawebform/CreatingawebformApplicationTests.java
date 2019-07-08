fb601ee7bcbf1ff67e09a264fbcb909656d3ed97
ansumana80 <ansurogersbusiness@gmail.com>
2019-07-08 12:01:50 -0400

    first commit

---
+++ src/test/java/com/example/creatingawebform/CreatingawebformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.creatingawebform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreatingawebformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
